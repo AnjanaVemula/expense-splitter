e48399c7bf376442724f740fa9c53a95d7034f14
Anjana Vemula <anjana.vemula10@gmail.com>
2026-07-19 18:56:35 +0530

    Add Balance model, fix Java version

---
+++ src/main/java/com/expensesplitter/model/Balance.java
@@ -0,0 +1,15 @@
+package com.expensesplitter.model;
+
+public class Balance {
+    private String name;
+    private double amount;
+
+    public Balance(String name, double amount) {
+        this.name = name;
+        this.amount = amount;
+    }
+
+    public String getName() { return name; }
+    public double getAmount() { return amount; }
+    public void setAmount(double amount) { this.amount = amount; }
+}
